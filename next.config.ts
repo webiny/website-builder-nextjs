6450c3bd4e24efdbbf26a5dd5ceb0354c539215a
Pavel Denisjuk <pavel@webiny.com>
2025-08-06 12:03:16 +0200

    chore: disable production sourcemaps

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     devIndicators: false,
+    productionBrowserSourceMaps: false,
     async headers() {
         return [
             {
