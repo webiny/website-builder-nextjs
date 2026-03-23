471111c187665293f889a0f70b8e0ab82376c506
adrians5j <adrian@webiny.com>
2026-03-23 13:03:01 +0100

    wip:7 (6b36c)

--- src/editorComponents/funnelBuilder/FunnelStep/FunnelStep.tsx
+++ src/editorComponents/funnelBuilder/FunnelStep/FunnelStep.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import type { ComponentProps } from "@webiny/website-builder-nextjs";
+import { useContainer } from "../FunnelContainer/ContainerProvider";
 
 type FunnelStepProps = ComponentProps<{
   label: string;
@@ -9,6 +10,10 @@ type FunnelStepProps = ComponentProps<{
 
 export function FunnelStep({ inputs }: FunnelStepProps) {
   // TODO: use context provider to determine the active step, and render `null` if not active.
+
+  const { funnelVm } = useContainer();
+
+  console.log("asds22adsd", funnelVm);
   return (
     <div data-component="Fub/Step">
       <div>{inputs.label}</div>
