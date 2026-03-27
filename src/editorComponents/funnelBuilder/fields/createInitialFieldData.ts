97bdc3828a8b8e67bf576f8944bafe4f7cbe6469
adrians5j <adrian@webiny.com>
2026-03-27 22:04:19 +0100

    wip: xcz32

--- src/editorComponents/funnelBuilder/fields/createInitialFieldData.ts
+++
@@ -1,6 +0,0 @@
-import { fieldFromDto } from "../models/fields/fieldFactory";
-
-export const createInitialFieldData = (fieldType: string) => {
-  const field = fieldFromDto({ type: fieldType, stepId: "" });
-  return field.toDto();
-};
