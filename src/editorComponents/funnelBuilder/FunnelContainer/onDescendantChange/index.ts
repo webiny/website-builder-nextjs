ee87dec24f1b77f1f3443a97d108e37336d1c503
adrians5j <adrian@webiny.com>
2026-03-27 22:04:29 +0100

    wip: xcz32

--- src/editorComponents/funnelBuilder/FunnelContainer/onDescendantChange/index.ts
+++ src/editorComponents/funnelBuilder/FunnelContainer/onDescendantChange/index.ts
@@ -1,5 +1,4 @@
 import { syncStepOnCreate } from "./syncStepOnCreate";
-import { syncStepOnUpdate } from "./syncStepOnUpdate";
 import { syncStepOnDelete } from "./syncStepOnDelete";
 import { syncFieldOnCreate } from "./syncFieldOnCreate";
 import { syncFieldOnUpdate } from "./syncFieldOnUpdate";
@@ -7,7 +6,6 @@ import { syncFieldOnDelete } from "./syncFieldOnDelete";
 
 export const onDescendantChange = [
   syncStepOnCreate,
-  syncStepOnUpdate,
   syncStepOnDelete,
   syncFieldOnCreate,
   syncFieldOnUpdate,
