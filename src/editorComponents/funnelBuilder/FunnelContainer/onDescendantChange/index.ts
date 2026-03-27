a179259477c959706e66109e4fa845a76775a674
adrians5j <adrian@webiny.com>
2026-03-28 00:16:10 +0100

    wip:32 (9e0tf)

--- src/editorComponents/funnelBuilder/FunnelContainer/onDescendantChange/index.ts
+++ src/editorComponents/funnelBuilder/FunnelContainer/onDescendantChange/index.ts
@@ -1,11 +1,5 @@
 import { syncStepOnCreate } from "./syncStepOnCreate";
 import { syncStepOnDelete } from "./syncStepOnDelete";
-import { syncFieldOnUpdate } from "./syncFieldOnUpdate";
 import { syncFieldOnDelete } from "./syncFieldOnDelete";
 
-export const onDescendantChange = [
-  syncStepOnCreate,
-  syncStepOnDelete,
-  syncFieldOnUpdate,
-  syncFieldOnDelete
-];
+export const onDescendantChange = [syncStepOnCreate, syncStepOnDelete, syncFieldOnDelete];
--- src/editorComponents/funnelBuilder/fields/checkboxGroup/index.ts
+++ src/editorComponents/funnelBuilder/fields/checkboxGroup/index.ts
@@ -1,4 +1,4 @@
-import { createComponent, createObjectInput } from "@webiny/website-builder-nextjs";
+import { createComponent } from "@webiny/website-builder-nextjs";
 import { FunnelCheckboxGroupField } from "./FunnelCheckboxGroupField";
 import { descendantOfFunnelStep } from "../../constraints";
 
@@ -7,30 +7,30 @@ export const funnelCheckboxGroupFieldComponent = createComponent(FunnelCheckboxG
   label: "Checkbox Group",
   image: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m424-312 282-282-56-56-226 226-114-114-56 56 170 170ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>`,
   group: "funnelBuilder",
-  tags: ["funnel-field"],
-  inputs: [
-    createObjectInput({
-      name: "fieldData",
-      hideFromUi: true,
-      fields: [],
-      defaultValue: () => {
-        // Copied from src/editorComponents/funnelBuilder/utils/getRandomId.ts
-        const getRandomId = () => Math.random().toString(36).substr(2, 7);
+  inputs: {},
+  onChange: ctx => {
+    if (ctx.action === "create") {
+      const step = ctx.getAncestor("Fub/Step")!;
+      const container = ctx.getAncestor("Fub/Container")!;
 
-        return {
-          id: getRandomId(),
-          fieldId: getRandomId(),
-          stepId: "",
+      container.updateInputs(inputs => {
+        inputs.containerData.fields.push({
+          id: ctx.id,
+          fieldId: ctx.id,
+          stepId: step.id,
           type: "checkboxGroup",
           label: "",
           helpText: "",
           validators: [],
           value: { type: "stringArray", array: true, value: [] },
           extra: { options: [] }
-        };
-      }
-    })
-  ],
+        });
+      });
+
+      // Execute command to open the settings dialog
+      ctx.executeCommand("Fub/OpenSettingsDialog", { elementId: ctx.id });
+    }
+  },
   canDelete: ctx => {
     // Cannot use const for name here b/c we need this fn to be serializable.
     const fieldId = ctx.getElementInputs().fieldData?.id;
